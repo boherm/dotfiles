c737732de9fcd32b1626fe348551fa04135f1ea0
boherm <boris.hermans@prestashop.com>
2024-06-27 13:14:30 +0200

    add tailwindcss support for nvim

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -15,15 +15,28 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
-        dependencies = { "L3MON4D3/LuaSnip" },
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "luckasRanarison/tailwind-tools.nvim",
+            "onsails/lspkind-nvim",
+        },
         config = function()
             local cmp = require'cmp'
+            local lspkind = require('lspkind')
             require("luasnip.loaders.from_vscode").lazy_load()
             require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
 
             cmp.setup({
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol',
+                        maxwidth = 50,
+                        ellipsis_char = '...',
+                        show_labelDetails = true,
+                        before = require("tailwind-tools.cmp").lspkind_format,
+                    }),
+                },
                 snippet = {
-                    -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
                         require('luasnip').lsp_expand(args.body)
                     end,
@@ -37,11 +50,11 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
-                    { name = 'luasnip' }, -- For luasnip users.
+                    { name = 'luasnip' },
                 }, {
                     { name = 'buffer' },
                 })
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,38 @@
 -- Highlight: Treesitter
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-            auto_install = true,
-			--ensure_installed = {
-			--	"vimdoc",
-			--	"lua",
-			--	"javascript",
-		    --	"astro",
-			--	"bash",
-			--	"csv",
-			--	"dockerfile",
-			--	"git_config",
-			--	"html",
-			--	"json",
-			--	"make",
-			--	"php",
-			--	"phpdoc",
-			--	"regex",
-			--	"scss",
-			--	"typescript",
-			--	"vue",
-			--},
-            highlight = { enable = true, disable = { "php" } },
-            indent = { enabled = false },
-		})
-	end,
+    {
+        "luckasRanarison/tailwind-tools.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {} -- your configuration
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                --ensure_installed = {
+                --	"vimdoc",
+                --	"lua",
+                --	"javascript",
+                --	"astro",
+                --	"bash",
+                --	"csv",
+                --	"dockerfile",
+                --	"git_config",
+                --	"html",
+                --	"json",
+                --	"make",
+                --	"php",
+                --	"phpdoc",
+                --	"regex",
+                --	"scss",
+                --	"typescript",
+                --	"vue",
+                --},
+                highlight = { enable = true, disable = { "php" } },
+                indent = { enabled = false },
+            })
+        end,
+    },
 }
