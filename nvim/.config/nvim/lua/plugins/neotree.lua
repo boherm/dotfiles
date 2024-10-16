24832904bfff0383d562b778e28fde8026e7c17e
boherm <boris.hermans@prestashop.com>
2024-10-16 13:00:41 +0200

    fix neotree conf in nvim

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,30 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+        require('neo-tree').setup {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        '.git',
+                        '.DS_Store',
+                        'thumbs.db',
+                        '.github',
+                        '.gitignore',
+                        'package-lock.json',
+                        'lazy-lock.json',
+                        'composer.lock',
+                        'go.sum',
+                    },
+                    never_show = {
+                        '.git'
+                    },
+                },
+            }
+        }
 		vim.keymap.set(
 			"n",
 			"<leader>e",
